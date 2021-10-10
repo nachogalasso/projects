ddeaffcbb352ed95d55e07503520da07d61786a2
Nakio82 <ignaciogalasso82@gmail.com>
2021-10-10 20:38:23 -0300

    links

---
+++ docs/survey_page/validation.js
@@ -0,0 +1,4 @@
+/* FORM ELEMENTS */
+const formValidate = document.getElementById('survey-form');
+const submitBtn = document.getElementById('submitBtn');
+const vEmail = document.getElementById('email');
